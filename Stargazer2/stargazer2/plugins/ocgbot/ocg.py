1126e21d0284ec4f5540e43196034e7692335c93
SuzukageHotori <Taburiss@Taburiss.com>
2024-03-18 20:21:09 +0800

    commit

--- Stargazer2/stargazer2/plugins/ocgbot/ocg.py
+++ Stargazer2/stargazer2/plugins/ocgbot/ocg.py
@@ -8,13 +8,18 @@ from nonebot.adapters.satori.event import *
 from nonebot.adapters.satori.message import *
 
 
+
 search_card = on_command("查卡", aliases={"ck", "CK"})
 
 @search_card.handle()
 async def _(bot: Bot, event: Event, state: T_State, args: Message = CommandArg()):
-    await bot.send(event, MessageSegment.text("Hello, world!"))
-    
+    await bot.send(event, MessageSegment.text("todo!!"))
+
+
 randomCard = on_command('随机一卡', aliases={'抽一张卡'})
+
+
+@randomCard.handle()
 async def _(bot: Bot, event: Event, state: T_State):
     groupSession = None
     sessionId = None
@@ -27,12 +32,12 @@ async def _(bot: Bot, event: Event, state: T_State):
         userType = 'group'
     try:
         userType = 'SU' if (str(event.user_id) in nonebot.get_driver().config.superusers) else userType
-        pm.CheckPermission(sessionId, groupSession, userType)
+        #pm.CheckPermission(sessionId, groupSession, userType)
     except PermissionError as e:
         await randomCard.finish(str(e))
     try:
         js = getRandomCard()
-        pm.UpdateLastSend(sessionId)
+        #pm.UpdateLastSend(sessionId)
     except Exception as e:
         await randomCard.finish("咿呀？卡组被送进异次元了呢~")
     await send3(js, randomCard)
